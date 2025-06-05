c1afa28bf07ca468d9b5df2fb0c5e2f93dd82596
Igor Mirsalikhov <imirsalikhov@floridapoly.edu>
2025-06-05 09:59:09 -0400

    Create test.cpp

---
+++ tests/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../PolyStudent.h"
+#include "../PolyStudent.h"
+
+int main(){
+    PolyStudent student;
+    std::cout << student.isHappy() << std::endl;
+
+    return 0;
+}
